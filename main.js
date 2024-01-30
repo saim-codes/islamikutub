aff4e6d58a5584d3fe87d5f110eac69b1d990a53
saim-codes <saimcodes@gmail.com>
2024-01-30 17:31:35 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector(".navbar");
+
+window.onscroll = () => {
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+
+function search_books() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('book');
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+}
+
